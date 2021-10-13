e7ac9560900112226738a801577076d322c4c859
Luca Cozzuto <lcozzuto@C02WX1XFHV2Q.local>
2021-10-13 20:39:08 +0200

    changing back

--- conf.py
+++ conf.py
@@ -16,8 +16,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-def setup(app):
-    app.add_css_file('custom.css')
 
 # -- Project information -----------------------------------------------------
 
